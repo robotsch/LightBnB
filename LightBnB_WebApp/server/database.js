fd4f1b284561abbdf5a1eca6f48615f64dee612a
robotsch <christianhumble22@gmail.com>
2022-04-07 14:26:22 -0400

    refactor getAllReservations to use database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -15,8 +15,7 @@ const getUserWithEmail = function (email) {
   return pool
     .query(`SELECT * FROM users WHERE email = $1`, values)
     .then((result) => {
-      console.log(result.rows);
-      return result.rows;
+      return result.rows[0];
     })
     .catch((err) => {
       console.log(err.message);
@@ -34,7 +33,7 @@ const getUserWithId = function (id) {
   return pool
     .query(`SELECT * FROM users WHERE id = $1`, values)
     .then((result) => {
-      return result.rows;
+      return result.rows[0];
     })
     .catch((err) => {
       console.log(err.message);
@@ -48,10 +47,6 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
   const values = [user.name, user.email, user.password];
   return pool
     .query(
@@ -63,7 +58,7 @@ const addUser = function (user) {
       values
     )
     .then((result) => {
-      return result.rows;
+      return result.rows[0];
     })
     .catch((err) => {
       console.log(err.message);
@@ -79,7 +74,27 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id];
+  return pool
+    .query(
+      `SELECT reservations.id, properties.title, properties.cost_per_night,
+      reservations.start_date, avg(rating) as average_rating, properties.number_of_bedrooms,
+      properties.number_of_bathrooms, properties.parking_spaces, thumbnail_photo_url
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT 10;`,
+      values
+    )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
@@ -101,7 +116,7 @@ const getAllProperties = (options, limit = 10) => {
       console.log(err.message);
     });
 };
-exports.getAllProperties = getAllReservations;
+exports.getAllProperties = getAllProperties;
 
 /**
  * Add a property to the database
